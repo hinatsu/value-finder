46a4f3348dbd1e086f06a8c74c2860f3d5fbdc58
Hinatsu Kusano <hinatsukusano@gmail.com>
2019-12-05 00:22:30 +0900

    wip

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import ValueFinder from "../views/ValueFinder.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: Home
+    name: "ValueFinder",
+    component: ValueFinder
   },
   {
     path: "/about",
---
+++ src/views/ValueFinder.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <draggable
+      :options="{ group: 'values' }"
+      element="ul"
+      v-for="(list, lIndex) in values"
+      :key="lIndex"
+    >
+      <li v-for="(item, index) in list" :key="index">{{ item }}</li>
+    </draggable>
+  </div>
+</template>
+
+<script lang="ts">
+const draggable = require("vuedraggable");
+
+export default {
+  components: {
+    draggable
+  },
+  data() {
+    return {
+      values: [
+        [
+          "honest",
+          "romance",
+          "change",
+          "existense",
+          "self-awareness",
+          "discovery",
+          "imagination"
+        ],
+        ["impact", "creation", "extraordinary"],
+        ["victory", "innovation", "future", "observant"]
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
