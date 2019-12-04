1c0d3bccfb9e06b3f52ec1c2d65f0ae54040a802
Hinatsu Kusano <hinatsukusano@gmail.com>
2019-12-05 08:39:54 +0900

    Add draggable

--- src/views/ValueFinder.vue
+++ src/views/ValueFinder.vue
@@ -1,13 +1,32 @@
 <template>
-  <div>
-    <draggable
-      :options="{ group: 'values' }"
-      element="ul"
-      v-for="(list, lIndex) in values"
-      :key="lIndex"
-    >
-      <li v-for="(item, index) in list" :key="index">{{ item }}</li>
-    </draggable>
+  <div >
+    <div class="col-12">
+
+      <draggable
+        :list="listCloud" group="values"
+        :key="lIndex"
+      >
+        <div v-for="(item, index) in listCloud" :key="index">{{ item }}</div>
+      </draggable>
+    </div>
+    <div class="row">
+
+    <div class="col-4">
+      <draggable :list="listCrutial" group="values">
+        <div v-for="(item, index) in listCrutial" :key="index">{{ item }}</div>
+      </draggable>
+    </div>
+    <div class="col-4">
+      <draggable :list="listImportant" group="values">
+        <div v-for="(item, index) in listImportant" :key="index">{{ item }}</div>
+      </draggable>
+    </div>
+    <div class="col-4"> 
+      <draggable :list="listNormal" group="values">
+        <div v-for="(item, index) in listNormal" :key="index">{{ item }}</div>
+      </draggable>
+    </div>
+    </div>
   </div>
 </template>
 
@@ -20,22 +39,40 @@ export default {
   },
   data() {
     return {
-      values: [
-        [
+      listCloud: [
+        
           "honest",
           "romance",
           "change",
           "existense",
           "self-awareness",
           "discovery",
-          "imagination"
-        ],
-        ["impact", "creation", "extraordinary"],
-        ["victory", "innovation", "future", "observant"]
-      ]
+          "imagination","impact", "creation", "extraordinary","victory", "innovation", "future", "observant"
+        
+      ],
+        listCrutial: [],
+        listImportant:[],
+        listNormal: []
     };
+  }, // data
+
+  methods: {
   }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.row {
+  .col-12 {
+    background-color: grey;
+  }
+  display: flex;
+  flex-direction: row;
+  .col-4 {
+    
+    background-color: grey;
+    min-height: 400px;
+    width: 100%;
+  }
+}
+</style>
