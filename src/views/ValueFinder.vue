cd382669b3cf41a9bcfc0acdb42ba6b8df86257b
hinatsu <hinatsu@spottedrecruit.com>
2019-12-05 10:16:59 +0900

    Import values

--- src/views/ValueFinder.vue
+++ src/views/ValueFinder.vue
@@ -1,63 +1,72 @@
 <template>
-  <div >
+  <div>
     <div class="col-12">
-
-      <draggable
-        :list="listCloud" group="values"
-        :key="lIndex"
-      >
-        <div v-for="(item, index) in listCloud" :key="index">{{ item }}</div>
+      <draggable :list="listCloud" group="values" :key="lIndex">
+        <span
+          class="value-cloud"
+          v-for="(value, index) in listCloud"
+          :key="index"
+          >{{ value.ja }}</span
+        >
       </draggable>
     </div>
     <div class="row">
-
-    <div class="col-4">
-      <draggable :list="listCrutial" group="values">
-        <div v-for="(item, index) in listCrutial" :key="index">{{ item }}</div>
-      </draggable>
-    </div>
-    <div class="col-4">
-      <draggable :list="listImportant" group="values">
-        <div v-for="(item, index) in listImportant" :key="index">{{ item }}</div>
-      </draggable>
-    </div>
-    <div class="col-4"> 
-      <draggable :list="listNormal" group="values">
-        <div v-for="(item, index) in listNormal" :key="index">{{ item }}</div>
-      </draggable>
-    </div>
+      <div class="col-4">
+        <draggable :list="listCrutial" group="values">
+          <div
+            class="value-crutial"
+            v-for="(value, index) in listCrutial"
+            :key="index"
+          >
+            {{ value.ja }}
+          </div>
+        </draggable>
+      </div>
+      <div class="col-4">
+        <draggable :list="listImportant" group="values">
+          <div
+            class="value-important"
+            v-for="(value, index) in listImportant"
+            :key="index"
+          >
+            {{ value.ja }}
+          </div>
+        </draggable>
+      </div>
+      <div class="col-4">
+        <draggable :list="listNormal" group="values">
+          <div
+            class="value-normal"
+            v-for="(value, index) in listNormal"
+            :key="index"
+          >
+            {{ value.ja }}
+          </div>
+        </draggable>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import { VALUES } from "@/constants";
 const draggable = require("vuedraggable");
 
 export default {
   components: {
     draggable
   },
+
   data() {
     return {
-      listCloud: [
-        
-          "honest",
-          "romance",
-          "change",
-          "existense",
-          "self-awareness",
-          "discovery",
-          "imagination","impact", "creation", "extraordinary","victory", "innovation", "future", "observant"
-        
-      ],
-        listCrutial: [],
-        listImportant:[],
-        listNormal: []
+      listCloud: VALUES,
+      listCrutial: [],
+      listImportant: [],
+      listNormal: []
     };
   }, // data
 
-  methods: {
-  }
+  methods: {} // methods
 };
 </script>
 
@@ -69,10 +78,28 @@ export default {
   display: flex;
   flex-direction: row;
   .col-4 {
-    
     background-color: grey;
     min-height: 400px;
     width: 100%;
+    > div {
+      height: 100%;
+      width: 100%;
+    }
+  }
+}
+
+.value {
+  &-cloud,
+  &-crutial,
+  &-important,
+  &-normal {
+    &:hover {
+      cursor: move;
+    }
+  }
+
+  &-cloud {
+    margin: 5px;
   }
 }
 </style>
